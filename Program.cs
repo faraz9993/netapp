d94c1afaabb4a036ea709114d979f6d591346068
Farajnazish Ansari <faraj.ansari@einfochips.com>
2025-01-01 15:49:41 +0530

    Updating Program.cs

--- Program.cs
+++ Program.cs
@@ -3,6 +3,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
-app.Urls.Add("http://0.0.0.0:5000");
+app.Urls.Add("https://farazwebapp-ebepfvhagvehhkb3.canadacentral-01.azurewebsites.net/");
 app.MapGet("/", () => Results.Ok("Hello World!"));
 app.Run();
